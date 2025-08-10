0f4c016cd6ba55a284810a292731258c8ca050e2
Kristof Evaert <kristofevaert@gmail.com>
2025-08-10 17:40:04 +0200

    fix 6

---
+++ scripts/serve.js
@@ -0,0 +1,77 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+
+const PORT = 8080;
+
+// MIME type mapping
+const mimeTypes = {
+  '.html': 'text/html',
+  '.js': 'application/javascript',
+  '.css': 'text/css',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpg',
+  '.gif': 'image/gif',
+  '.svg': 'image/svg+xml',
+  '.wav': 'audio/wav',
+  '.mp4': 'video/mp4',
+  '.woff': 'application/font-woff',
+  '.ttf': 'application/font-ttf',
+  '.eot': 'application/vnd.ms-fontobject',
+  '.otf': 'application/font-otf',
+  '.wasm': 'application/wasm'
+};
+
+const server = http.createServer((req, res) => {
+  // Parse URL
+  const parsedUrl = url.parse(req.url);
+  let pathname = parsedUrl.pathname;
+  
+  // Default to index.html for root
+  if (pathname === '/') {
+    pathname = '/index.html';
+  }
+  
+  // Get file path
+  const filePath = path.join(__dirname, '../dist', pathname);
+  
+  // Get file extension
+  const extname = path.extname(filePath);
+  const contentType = mimeTypes[extname] || 'application/octet-stream';
+  
+  // Read file
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        // File not found, serve index.html for SPA routing
+        fs.readFile(path.join(__dirname, '../dist/index.html'), (err, data) => {
+          if (err) {
+            res.writeHead(404);
+            res.end('File not found');
+          } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+          }
+        });
+      } else {
+        res.writeHead(500);
+        res.end('Server error');
+      }
+    } else {
+      // Set CORS headers
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+      
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(data);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+  console.log('Press Ctrl+C to stop');
+});
